174d5d1e30aab279702d52d56a5845027cbbb282
remixalpha <swapnarahul170@gmail.com>
2023-09-07 20:13:37 +0530

    sign in page modified

--- frontend/src/components/Signup/index.js
+++ frontend/src/components/Signup/index.js
@@ -1,4 +1,6 @@
+import React, { useState } from "react";
 import Logo from "../../assets/images/logos/ipt.png";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { postLogin } from "../../utils/agent";
 import { Formik } from "formik";
 import * as Yup from "yup";
@@ -9,44 +11,22 @@ const SignupSchema = Yup.object().shape({
 });
 export default function SignUp() {
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <>
-      <div className="flex flex-col items-center justify-center flex-1 min-h-screen px-6 py-12 bg-white lg:px-8 isolate ">
-        <div className="absolute inset-0 overflow-hidden -z-10">
-          <svg
-            className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
-            aria-hidden="true"
-          >
-            <defs>
-              <pattern
-                id="e813992c-7d03-4cc4-a2bd-151760b470a0"
-                width={200}
-                height={200}
-                x="50%"
-                y="50%"
-                patternUnits="userSpaceOnUse"
-              >
-                <path d="M100 200V.5M.5 .5H200" fill="none" />
-              </pattern>
-            </defs>
-
-            <rect
-              width="60%"
-              height="100%"
-              strokeWidth={0}
-              fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"
-            />
-          </svg>
-        </div>
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="flex flex-col items-center justify-center flex-1 min-h-screen px-6 py-12 bg-black lg:px-8 isolate  ">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm relative -top-10 space-y-20 mb-5 ">
           <img
             className="w-auto h-10 mx-auto scale-150 "
             src={Logo}
             alt="IPT & GPTC"
           />
-          <h2 className="mt-10 text-2xl font-bold leading-9 text-center ">
+          {/* <h2 className=" text-white text-4xl font-bold leading-9 text-center ">
             Login
-          </h2>
+          </h2> */}
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -90,11 +70,14 @@ export default function SignUp() {
 
               /* and other goodies */
             }) => (
-              <form className="space-y-6" onSubmit={handleSubmit}>
+              <form
+                className="space-y-6 bg-lightPrimary rounded-primary p-10 scale-125 bg-opacity-25  "
+                onSubmit={handleSubmit}
+              >
                 <div>
                   <label
                     htmlFor="email"
-                    className="block text-sm font-medium leading-6 text-gray-900"
+                    className="block text-sm font-medium leading-6 text-white"
                   >
                     Email address
                   </label>
@@ -104,11 +87,12 @@ export default function SignUp() {
                       name="email"
                       type="email"
                       autoComplete="email"
+                      placeholder="example@gmail.com"
                       required
                       onChange={handleChange}
                       onBlur={handleBlur}
                       value={values.email}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 pl-4 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
                     />
                   </div>
                   {errors.email && touched.email && errors.email}
@@ -118,14 +102,14 @@ export default function SignUp() {
                   <div className="flex items-center justify-between">
                     <label
                       htmlFor="password"
-                      className="block text-sm font-medium leading-6 text-gray-900"
+                      className="block text-sm font-medium leading-6 text-white"
                     >
                       Password
                     </label>
                     <div className="text-sm">
                       <a
                         href="#"
-                        className="font-semibold text-indigo-600 hover:text-indigo-500"
+                        className="font-semibold text-orange-600 hover:text-orange-500"
                       >
                         Forgot password?
                       </a>
@@ -135,14 +119,27 @@ export default function SignUp() {
                     <input
                       id="password"
                       name="password"
-                      type="password"
+                      type={showPassword ? "text" : "password"}
                       onChange={handleChange}
                       onBlur={handleBlur}
                       value={values.password}
                       autoComplete="current-password"
                       required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 pl-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400   sm:text-sm sm:leading-6"
                     />
+                    <div className="absolute inset-y-0 right-0 pt-16 pr-12 flex items-center cursor-pointer ">
+                      {showPassword ? (
+                        <AiOutlineEyeInvisible
+                          onClick={togglePasswordVisibility}
+                        >
+                          visibility_off
+                        </AiOutlineEyeInvisible>
+                      ) : (
+                        <AiOutlineEye onClick={togglePasswordVisibility}>
+                          visibility
+                        </AiOutlineEye>
+                      )}
+                    </div>
                   </div>
                   {errors.password && touched.password && errors.password}
                 </div>
@@ -152,7 +149,7 @@ export default function SignUp() {
                     // type="submit"
                     type="submit"
                     disabled={isSubmitting}
-                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="flex w-full justify-center rounded-primary bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-xl hover:bg-orange-400 "
                   >
                     Login
                   </button>
